04acf56b64dab1e950ffa349c80b79a88a65d175
FLorine-R28 <rodriguesflorine88pgmail.com>
2022-11-14 12:42:07 +0100

    complete a task in localstorage

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const addTaskLocalStorage = (taskValue) => {
     let tasks = JSON.parse(localStorage.getItem('tasks'));/*string transformé en objet*/
 
     if (tasks /*!== null*/) { /* dans le cas où on a pas de local storage*/
-        tasks.push(taskValue);
+        tasks.push({ value : taskValue, isCompleted: false });
     } else {
-        tasks = [taskValue];
+        tasks = ({ value : taskValue, isCompleted: false });
     }
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -22,23 +22,39 @@ const removeTaskInLocalstorage = (taskIndex /*sa position dans le tableau*/) => 
     localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
+const toggleCompletedTaskInLocalStorage = (taskIndex, isCompleted) /* on va lui passer l'index de la tache à modif*/ => {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (tasks) {
+        tasks[taskIndex].isCompleted = isCompleted;
+    } else {
+        tasks = [];
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 const removeTask = (event, index) => /*function removeTask*/ {
     removeTaskInLocalstorage(index);
     const taskListContainer = document.getElementById('taskListContainer');
     taskListContainer.removeChild(event.target.parentNode);
 };
 
-const completeTask = (event) => {
+const completeTask = (event, index) => {
     if (event.target.parentNode.classList.contains('completed')) {
         event.target.parentNode.classList.remove('completed');
+        toggleCompletedTaskInLocalStorage(index, false);
     } else {
         event.target.parentNode.classList.add('completed');
+        toggleCompletedTaskInLocalStorage(index, true);
     }
 }
 
-const createTaskListElement = (value, index) => {
+const createTaskListElement = (value, index, isCompleted = false) => {
     const taskListElement = document.createElement('li');
     taskListElement.classList.add('task-element');
+    /*if (isCompleted) {taskListElement.classList.add('completed')}*/
+    isCompleted && taskListElement.classList.add('completed');
 
     const labelElement = document.createElement('span');
     labelElement.classList.add('task-label');
@@ -50,7 +66,7 @@ const createTaskListElement = (value, index) => {
     completeBtnElement.classList.add('btn', 'complete-task-btn');
     completeBtnElement.title = 'Terminer la tâche';
     completeBtnElement.innerHTML = '<i class="fa-solid fa-check"></i>'; 
-    completeBtnElement.addEventListener('click', completeTask);
+    completeBtnElement.addEventListener('click', (event) => completeTask(event, index));
 
     taskListElement.appendChild(completeBtnElement);
 
@@ -86,7 +102,7 @@ const init = () => {
         const taskListContainer = document.getElementById('taskListContainer');
 
         tasks.forEach((task, index) => {
-            const taskListElement =  createTaskListElement(task,index);
+            const taskListElement =  createTaskListElement(task.value,index, task.isCompleted);
             taskListContainer.appendChild(taskListElement);
         })
     }
